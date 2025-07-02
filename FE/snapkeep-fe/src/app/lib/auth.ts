273b7d898499975921e293317c17bb0ebaef40bc
quangvungo <quangvu221004@gmail.com>
2025-07-02 12:25:24 +0700

    update

--- FE/snapkeep-fe/src/app/lib/auth.ts
+++
@@ -1,9 +0,0 @@
-export const saveToken = (token: string) => {
-    localStorage.setItem('accessToken', token);
-}
-export const getToken = () => {
-    return localStorage.getItem('accessToken');
-}
-export const clearToken = () => {
-    return localStorage.removeItem('accessToken');
-}
